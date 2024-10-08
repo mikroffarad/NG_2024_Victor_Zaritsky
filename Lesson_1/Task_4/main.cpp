be4c99e3b34a8eae2af28004b347dbb499765581
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2024-10-08 21:06:59 +0000

    added Task_4

---
+++ Lesson_1/Task_4/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int salary;
+    cout << "Enter your salary: ";
+    cin >> salary;
+
+    if (salary < 1000) {
+        cout << "Work more and harder!" << endl;
+    }
+
+    if (salary > 999) {
+
+        if (salary < 1000000) {
+            cout << "Well done!" << endl;
+        }
+    }
+
+    if (salary > 999999) {
+        cout << "You're a millioner!" << endl;
+    }
+}
