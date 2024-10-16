e1e0aaa311b66df2d11bbdf437359f1580ba18d1
Victor Zaritsky (mikroffarad) <viktorzarickiy@gmail.com>
2024-10-16 17:52:16 +0000

    Fixed Task_2 for Lesson_2

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -4,39 +4,25 @@ using namespace std;
 
 int main()
 {
-    int MAX_INPUTS = 20;
+    int MAX_INPUTS = 5;
     int values[MAX_INPUTS];
-
-    int maxValue = 0;
-    int maxEnteredInputs = 0;
 
     for (int counter = 0; counter < MAX_INPUTS; counter++) {
         cout << "Enter " << counter + 1 << " value: ";
         cin >> values[counter];
-
-        if (values[counter] > maxValue) {
-            maxValue = values[counter];
-        }
-
-        if (values[counter] == 0) {
-            maxEnteredInputs = counter;
-            break;
-        }
     }
 
-    for (int counterValues = 0; counterValues < maxEnteredInputs; counterValues++) {
-
-        int spaces = (maxValue - values[counterValues]) / 2;
-
-        for (int counterSpaces = 0; counterSpaces < spaces; counterSpaces++) {
+    for (int row = 1; row <= MAX_INPUTS; row++) {
+        for (int counter = 0; counter < MAX_INPUTS; counter++) {
+            if (values[counter] >= row) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
             cout << " ";
         }
-
-        for (int counterStars = 0; counterStars < values[counterValues]; counterStars++) {
-            cout << "*";
-        }
-
         cout << endl;
     }
 
+    return 0;
 }
