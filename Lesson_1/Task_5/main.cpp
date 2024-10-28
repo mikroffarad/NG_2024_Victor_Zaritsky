c2b1622b2d725baca9686be89098d0d6451efcf5
Victor Zaritsky <viktorzarickiy@gmail.com>
2024-10-28 15:14:01 +0000

    refactored triangle output in Lesson_1/Task_5

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -1,32 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int treeHeight = 0;
+int main() {
+    int treeHeight;
     cout << "Enter a number: ";
     cin >> treeHeight;
 
-    for (int currentRow = 1; currentRow <= treeHeight + 1; currentRow++) {
+    for(int currentRow = 0; currentRow < treeHeight; currentRow++) {
 
-        int maxWidth = (treeHeight * 2 - 1);
-        int starsCount = 2 * currentRow - 1;
-
-        if (currentRow - treeHeight == 1) {
-            starsCount = 1;
-        }
-
-        int spacesCount = (maxWidth - starsCount) / 2;
-
-        for (int spaceIndex = 0; spaceIndex < spacesCount; spaceIndex++) {
+        for(int spaceIndex = 0; spaceIndex < treeHeight-currentRow-1; spaceIndex++)
             cout << " ";
-        }
 
-        for (int starIndex = 0; starIndex < starsCount; starIndex++) {
+        for(int starIndex = 0; starIndex < 2*currentRow+1; starIndex++)
             cout << "*";
-        }
-
         cout << endl;
     }
+
+    for(int spaceIndex = 0; spaceIndex < treeHeight-1; spaceIndex++)
+        cout << " ";
+    cout << "*" << endl;
+
+    return 0;
 }
