e765374fef69ec433cab58dd614e5a06d4e1be14
mikroffarad <viktorzarickiy@gmail.com>
2024-11-19 22:04:24 +0200

    Made Task_1 in Lesson_4

--- Lesson_4/Task_1/bank.h
+++ Lesson_4/Task_1/bank.h
@@ -1,8 +1,6 @@
 #ifndef BANK_H
 #define BANK_H
 
-#endif // BANK_H
-
 #include <iostream>
 #include <string>
 #include <map>
@@ -10,27 +8,52 @@ using namespace std;
 
 class Bank {
 private:
-    struct accountInfo {
+    struct AccountInfo {
         float balance;
         float credit;
         float deposit;
+        int depositDate;
+        int creditDate;
+        float depositRate;
+        float creditRate;
 
-        accountInfo() : balance(0), credit(0), deposit(0) {}
+        AccountInfo()
+            : balance(0), credit(0), deposit(0), depositDate(0), creditDate(0), depositRate(0), creditRate(0) {}
     };
 
-    map<string, accountInfo> accounts;
+    map<string, AccountInfo> accounts;
+    int currentDate; // Stores the current date as an integer (e.g., YYYYMMDD)
 
-    int currentDate;
+    void calculateInterest(AccountInfo &account, int days) {
+        if (account.deposit > 0 && account.depositDate > 0) {
+            account.balance += account.deposit * (account.depositRate / 365) * days;
+        }
+        if (account.credit > 0 && account.creditDate > 0) {
+            account.credit += account.credit * (account.creditRate / 365) * days;
+        }
+    }
 
 public:
     Bank() : currentDate(0) {}
 
+    void setDate(int newDate) {
+        if (newDate <= currentDate) {
+            cout << "Invalid date. The new date must be in the future." << endl;
+            return;
+        }
+        int daysPassed = newDate - currentDate;
+        for (auto &entry : accounts) {
+            calculateInterest(entry.second, daysPassed);
+        }
+        currentDate = newDate;
+    }
+
     bool createAccount(string accountID, float initialBalance) {
         if (accounts.find(accountID) == accounts.end()) {
             accounts[accountID].balance = initialBalance;
-            return true; // Account created successfully
+            return true;
         }
-        return false; // Account already created
+        return false;
     }
 
     bool deleteAccount(string accountID) {
@@ -40,20 +63,19 @@ public:
     void showAccountInfo(string accountID) {
         auto it = accounts.find(accountID);
         if (it != accounts.end()) {
-            accountInfo info = it->second;
+            AccountInfo &info = it->second;
             cout << "Account ID: " << accountID << endl
                  << "Balance: " << info.balance << endl
                  << "Credit: " << info.credit << endl
-                 << "Deposit: " << info.credit << endl;
+                 << "Deposit: " << info.deposit << endl
+                 << "Current Date: " << currentDate << endl;
         } else {
             cout << "Account not found!" << endl;
         }
     }
 
     bool deposit(string accountID, float amount) {
-        if (amount <= 0) {
-            return false;
-        }
+        if (amount <= 0) return false;
 
         auto it = accounts.find(accountID);
         if (it != accounts.end()) {
@@ -64,16 +86,67 @@ public:
     }
 
     bool withdraw(string accountID, float amount) {
-        if (amount <= 0) {
-            return false;
-        }
+        if (amount <= 0) return false;
 
         auto it = accounts.find(accountID);
-        if (it != accounts.end()) {
+        if (it != accounts.end() && it->second.balance >= amount) {
             it->second.balance -= amount;
             return true;
         }
         return false;
     }
 
+    bool takeCredit(string accountID, float amount, float annualInterestRate) {
+        if (amount <= 0 || annualInterestRate <= 0) return false;
+
+        auto it = accounts.find(accountID);
+        if (it != accounts.end()) {
+            it->second.credit += amount;
+            it->second.creditDate = currentDate;
+            it->second.balance += amount;
+            it->second.creditRate = annualInterestRate / 100;
+            return true;
+        }
+        return false;
+    }
+
+    bool returnCredit(string accountID, float amount) {
+        if (amount <= 0) return false;
+
+        auto it = accounts.find(accountID);
+        if (it != accounts.end() && it->second.credit >= amount) {
+            it->second.credit -= amount;
+            it->second.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    bool placeDeposit(string accountID, float amount, float annualInterestRate) {
+        if (amount <= 0 || annualInterestRate <= 0) return false;
+
+        auto it = accounts.find(accountID);
+        if (it != accounts.end() && it->second.balance >= amount) {
+            it->second.deposit += amount;
+            it->second.depositDate = currentDate;
+            it->second.balance -= amount;
+            it->second.depositRate = annualInterestRate / 100;
+            return true;
+        }
+        return false;
+    }
+
+    bool withdrawDeposit(string accountID, float amount) {
+        if (amount <= 0) return false;
+
+        auto it = accounts.find(accountID);
+        if (it != accounts.end() && it->second.deposit >= amount) {
+            it->second.deposit -= amount;
+            it->second.balance += amount;
+            return true;
+        }
+        return false;
+    }
 };
+
+#endif // BANK_H
--- Lesson_4/Task_1/main.cpp
+++ Lesson_4/Task_1/main.cpp
@@ -6,16 +6,22 @@ using namespace std;
 int main() {
     Bank bank;
 
+    bank.setDate(20241101);
     bank.createAccount("ACC001", 1000);
-
     bank.showAccountInfo("ACC001");
 
     bank.deposit("ACC001", 500);
-
     bank.showAccountInfo("ACC001");
 
     bank.withdraw("ACC001", 300);
+    bank.showAccountInfo("ACC001");
 
+    bank.takeCredit("ACC001", 200, 10.0);
+    bank.setDate(20241115);
+    bank.showAccountInfo("ACC001");
+
+    bank.placeDeposit("ACC001", 700, 5.0);
+    bank.setDate(20241119);
     bank.showAccountInfo("ACC001");
 
     return 0;
